0d497622f93208a84d5709870143ffccb681ccac
Chisom Anumba <chisom213@gmail.com>
2023-01-16 15:35:15 -0500

    sWAPcASE2.py

---
+++ sWAP cASE/sWAPcASE2.py
@@ -0,0 +1,7 @@
+def swap_case(s):
+    return ''.join([i.lower() if i.isupper() else i.upper() for i in s])
+  
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
