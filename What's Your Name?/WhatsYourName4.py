9ec7e5f3d4fb1875d9dd8da25b65356427622144
Chisom Anumba <chisom213@gmail.com>
2023-01-17 23:16:11 -0500

    WhatsYourName4.py

--- What's Your Name?/WhatsYourName4.py
+++ What's Your Name?/WhatsYourName4.py
@@ -1,5 +1,5 @@
 def print_full_name(first, last):
-    print('Hello ' + first + ' ' + last + '! You just delved into python.')
+    print('Hello ' + first, last + '! You just delved into python.')
     
 if __name__ == '__main__':
     first_name = input()
