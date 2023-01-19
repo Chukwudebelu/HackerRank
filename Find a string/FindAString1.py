75fb0335a43887c65ac9bd5b2a8f6a9f9b27a04f
Chisom Anumba <chisom213@gmail.com>
2023-01-19 05:28:09 -0500

    FindAString1.py

---
+++ Find a string/FindAString1.py
@@ -0,0 +1,10 @@
+def count_substring(string: str, sub_string: str) -> int:
+  m, n = len(sub_string), len(string)
+  return len([i for i in range(n-m+1) if string[i:i+m]==sub_string])
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
