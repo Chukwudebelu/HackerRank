78ea1cc7e6a87beeabc6eb60c14e92e612a8b85d
Chisom Anumba <chisom213@gmail.com>
2023-01-20 13:00:39 -0500

    StringValidator3.py

---
+++ String Validators/StringValidator3.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    s = input()
+    print(any(map(str.isalnum, s)))
+    print(any(map(str.isalpha, s)))
+    print(any(map(str.isdigit, s)))
+    print(any(map(str.islower, s)))
+    print(any(map(str.isupper, s)))
