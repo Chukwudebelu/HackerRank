d1588bf78131c44650e8e5f6d5f2282ad63b1146
Chisom Anumba <chisom213@gmail.com>
2023-01-22 02:40:14 -0500

    TextWrap3.py

---
+++ Text-Wrap/TextWrap3.py
@@ -0,0 +1,11 @@
+import textwrap
+
+def wrap(string, max_width):
+    for i in range(1, len(string) // max_width + 1):
+        string = string[:i*max_width+i-1] + '\n' + string[i*max_width+i-1:]
+    return string
+    
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
