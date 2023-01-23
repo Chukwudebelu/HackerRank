06704d0b69858aba5bf5f631e634ef86c72606b0
Chisom Anumba <chisom213@gmail.com>
2023-01-23 14:14:28 -0500

    JavaStdinStdout0.py
    Task
    In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
    
    Input Format
    There are  lines of input, and each line contains a single integer.

---
+++ Java/Introduction/Java-Stdin-and-Stdout-I/JavaStdinStdout0.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a + "\n" + b + "\n" + c);
+    }
+}
