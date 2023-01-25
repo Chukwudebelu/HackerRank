9d65c64f228db5984da6c7e250189d17a71afd43
Chisom Anumba <chisom213@gmail.com>
2023-01-25 11:16:03 -0500

    FindThePoint1.py

---
+++ Mathematics/Fundamentals/Find-the-Point/FindThePoint1.py
@@ -0,0 +1,20 @@
+def findPoint(px, py, qx, qy):
+    return [(qx - px) + qx, (qy - py) + qy]
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    for n_itr in range(n):
+        first_multiple_input = input().rstrip().split()
+
+        px = int(first_multiple_input[0])
+
+        py = int(first_multiple_input[1])
+
+        qx = int(first_multiple_input[2])
+
+        qy = int(first_multiple_input[3])
+
+        result = findPoint(px, py, qx, qy)
+
+        print(' '.join(map(str, result)))
