ebfd0e2929de05d1d0a11b322dcd2632319c96f3
Chisom Anumba <chisom213@gmail.com>
2023-01-26 09:41:49 -0500

    HurdleRace3.py

---
+++ Problem-Solving/Algorithms/Implementation/The-Hurdle-Race/HurdleRace3.py
@@ -0,0 +1,16 @@
+def hurdleRace(k, height):
+  height.sort()
+  return max(0, height[-1] - k)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    print(result)
