5d2c0aeb5c2809c3cc3908781ca4ecb289477d39
Chisom Anumba <chisom213@gmail.com>
2023-01-26 09:47:51 -0500

    HurdleRace8.py

---
+++ Problem-Solving/Algorithms/Implementation/The-Hurdle-Race/HurdleRace8.py
@@ -0,0 +1,15 @@
+def hurdleRace(k, height):
+    return max(0, sorted(height, reverse=True)[0]-k)
+  
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    print(result)
