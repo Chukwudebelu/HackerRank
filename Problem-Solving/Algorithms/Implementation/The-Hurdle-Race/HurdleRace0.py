b4c3bdc2496d4af6e6c75755c895be26e4f9489d
Chisom Anumba <chisom213@gmail.com>
2023-01-26 09:37:15 -0500

    HurdleRace0.py
    A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0.
    
    Example
    height = [1, 2, 3, 3, 2]
    k = 1
    The character can jump 1 unit high initially and must take 3 - 1 = 2 doses of potion to be able to jump all of the hurdles.
    
    Function Description
    Complete the hurdleRace function in the editor below.
    hurdleRace has the following parameter(s):
    - int k: the height the character can jump naturally
    - int height[n]: the heights of each hurdle
    
    Returns
    - int: the minimum number of doses required, always 0 or more
    
    Input Format
    The first line contains two space-separated integers n and k, the number of hurdles and the maximum height the character can jump naturally.
    The second line contains n space-separated integers height[i] where 0 <= i < n.
    
    Constraints
    - 1 <= n, k <= 100
    - 1 <= height[i] <= 100

---
+++ Problem-Solving/Algorithms/Implementation/The-Hurdle-Race/HurdleRace0.py
@@ -0,0 +1,17 @@
+def hurdleRace(k, height):
+    lst = [i-k for i in height if i > k]
+    return max(lst) if len(lst) != 0 else 0
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    print(result)
