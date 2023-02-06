0082d87a1fde1f8ac6eb8ccaaedc632acccf7edd
Chisom Anumba <chisom213@gmail.com>
2023-02-05 21:52:56 -0500

    SetUnion0.py
    Task
    The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
    
    You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
    
    Input Format
    
    The first line contains an integer, n, the number of students who have subscribed to the English newspaper.
    The second line contains n space separated roll numbers of those students.
    The third line contains b, the number of students who have subscribed to the French newspaper.
    The fourth line contains b space separated roll numbers of those students.
    
    Constraints
    - 0 < Total number of students in college < 1000

---
+++ Python/Set/Set.union()/SetUnion0.py
@@ -0,0 +1,16 @@
+def union_all(A, B):
+    A_set = set(A)
+    B_set = set(B)
+    return len(A_set | B_set)
+
+if __name__ == "__main__":
+    num_eng_paper = int(input())
+    
+    eng_paper = list(map(int, input().split()))
+
+    num_fre_paper = int(input())
+
+    fre_paper = list(map(int, input().split()))
+    
+    result = union_all(eng_paper, fre_paper)
+    print(result)
