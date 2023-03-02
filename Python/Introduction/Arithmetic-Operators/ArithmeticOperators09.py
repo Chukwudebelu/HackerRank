819dc6e3128704f104a390b2d0832336a5fb566a
Chisom Anumba <chisom213@gmail.com>
2023-03-01 21:28:31 -0500

    ArithmeticOperators09.py

---
+++ Python/Introduction/Arithmetic-Operators/ArithmeticOperators09.py
@@ -0,0 +1,16 @@
+def add2Nums(x, y):
+  return x + y
+
+def sub2Nums(x, y):
+  return x - y
+
+def prod2Nums(x, y):
+  return x * y
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(add2Nums(a,b))
+    print(sub2Nums(a,b))
+    print(prod2Nums(a,b))
