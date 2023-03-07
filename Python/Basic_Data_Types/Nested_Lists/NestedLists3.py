1b6e5eca3815d3065538dace65c3bd931c301781
Chisom Anumba <chisom213@gmail.com>
2023-03-07 09:52:46 -0500

    NestedLists3.py

---
+++ Python/Basic_Data_Types/Nested_Lists/NestedLists3.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    N = int(input())
+    records = [[input(), float(input())] for _ in range(N)]
+    lst = [rec for rec in records if rec[1] != min([char[1] for char in records])]
+    lst2 = [valid[0] for valid in lst if valid[1] == min([i[1] for i in lst])]
+    print(*sorted(lst2), sep='\n')
