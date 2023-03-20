43a96c16a09c584451d456fd263c10d74fa74558
Chisom Anumba <chisom213@gmail.com>
2023-03-20 14:26:57 -0400

    JavaLoopsII1.java

---
+++ Java/Introduction/Java_Loops_II/JavaLoopsII1.java
@@ -0,0 +1,19 @@
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int i = 0; i < q; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            for (int j = 1; j <= n; j++){
+              a += b;
+              System.out.print(a + " ");
+              b *= 2;
+            }
+            System.out.print("\n");
+        }
+        in.close();
+    }
+}
