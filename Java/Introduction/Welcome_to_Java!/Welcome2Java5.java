32be874f688a0e9cec1ee2069e32f3f1192e9006
Chisom Anumba <chisom213@gmail.com>
2023-03-20 14:42:22 -0400

    Welcome2Java5.java

---
+++ Java/Introduction/Welcome_to_Java!/Welcome2Java5.java
@@ -0,0 +1,8 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.printf("Hello, World.");
+        System.out.printf("\nHello, Java.");
+    }
+}
