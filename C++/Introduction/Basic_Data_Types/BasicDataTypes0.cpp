882bb74ebc0bc8bc16c31a9b55a88339d86875e4
Chisom Anumba <chisom213@gmail.com>
2023-03-20 23:53:56 -0400

    BasicDataTypes0.cpp
    Some C++ data types, their format specifiers, and their most common bit widths are as follows:
    
    - Int ("%d"): 32 Bit integer
    - Long ("%ld"): 64 bit integer
    - Char ("%c"): Character type
    - Float ("%f"): 32 bit real value
    - Double ("%lf"): 64 bit real value

---
+++ C++/Introduction/Basic_Data_Types/BasicDataTypes0.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a; std::cin >> a;
+    long b; std::cin >> b;
+    char c; std::cin >> c;
+    float d; std::cin >> d;
+    double e; std::cin >> e;
+    // scanf("%d %ld %c")
+    printf("%d\n%ld\n%c\n%f\n%lf", a, b, c, d, e);
+    return 0;
+}
