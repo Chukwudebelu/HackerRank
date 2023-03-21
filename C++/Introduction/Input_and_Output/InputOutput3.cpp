12ae935d554b80a529c21470d3de73ff6031e584
Chisom Anumba <chisom213@gmail.com>
2023-03-20 23:37:17 -0400

    InputOutput3.cpp

---
+++ C++/Introduction/Input_and_Output/InputOutput3.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a, b, c; 
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", a+b+c);
+    return 0;
+}
