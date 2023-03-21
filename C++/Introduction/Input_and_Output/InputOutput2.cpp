2814dba3108ead31ff516a4963e1c2d49ccdc716
Chisom Anumba <chisom213@gmail.com>
2023-03-20 23:31:05 -0400

    InputOutput2.cpp

---
+++ C++/Introduction/Input_and_Output/InputOutput2.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a; cin >> a;
+    int b; cin >> b;
+    int c; cin >> c;
+    b += c;
+    a += b;
+    std::cout << a << endl;
+    return 0;
+}
