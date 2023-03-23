fac1c6d8eb8be6216f3a944aa47e01b418dafa80
Chisom Anumba <chisom213@gmail.com>
2023-03-23 17:28:54 -0400

    Welcome2Java7.java

---
+++ Java/Introduction/Welcome_to_Java!/Welcome2Java7.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        String msg1 = "Hello, World.";
+        String msg2 = "Hello, Java.";
+        System.out.println(msg1);
+        System.out.println(msg2);
+    }
+}
