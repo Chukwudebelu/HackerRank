8af3366b6ef6133b780be7921124dac5cba05516
Chisom Anumba <chisom213@gmail.com>
2023-03-26 12:42:44 -0400

    JavaSubstring2.java

---
+++ Java/Introduction/Java_Substring/JavaSubstring2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String substr = "";
+        while (start < end) {
+          substr += S.charAt(start);
+          start++;
+        }
+        System.out.print(substr);
+    }
+}
