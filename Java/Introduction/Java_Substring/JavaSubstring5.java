f1923ee631ecdd80d4681792b1e0054a229df096
Chisom Anumba <chisom213@gmail.com>
2023-03-26 12:44:29 -0400

    JavaSubstring5.java

---
+++ Java/Introduction/Java_Substring/JavaSubstring5.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.next().substring(in.nextInt(), in.nextInt()));
+        in.close();
+    }
+}
