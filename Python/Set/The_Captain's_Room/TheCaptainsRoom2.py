149d81a6472480691bf1301399d58ee22750a06c
Chisom Anumba <chisom213@gmail.com>
2023-04-07 14:15:03 -0400

    TheCaptainsRoom2.py

--- Python/Set/The_Captain's_Room/TheCaptainsRoom2.py
+++ Python/Set/The_Captain's_Room/TheCaptainsRoom2.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
   K = int(input().strip(' ')) 
   l1 = list(map(int, input().split(' ')))
    
-  for room_num, count in groupby(sorted(l1)): 
-    if len(list(count)) != K:
+  for room_num, room_list_count in groupby(sorted(l1)): 
+    if len(list(room_list_count)) != K:
       print(room_num)
