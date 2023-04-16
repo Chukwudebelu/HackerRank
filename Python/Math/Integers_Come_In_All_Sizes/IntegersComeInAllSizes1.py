c73897c42752712f09a077f4dff41fc651fb0a12
Chisom Anumba <chisom213@gmail.com>
2023-04-16 11:06:00 -0400

    IntegersComeInAllSizes1.py

---
+++ Python/Math/Integers_Come_In_All_Sizes/IntegersComeInAllSizes1.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == "__main__":
+    print(sum([pow(int(input()),int(input())), pow(int(input()), int(input()))]))
