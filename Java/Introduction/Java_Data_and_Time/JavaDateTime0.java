edfe886b1342ce10ab79f5feaf3fe3a0824d6338
Chisom Anumba <chisom213@gmail.com>
2023-05-03 12:36:22 -0400

    Create JavaDateTime0.java
    The Calendar class (https://docs.oracle.com/javase/7/docs/api/java/util/Calendar.html) is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
    
    You are given a date. You just need to write the method, , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
    
    Function Description
    Complete the findDay function in the editor below.
    findDay has the following parameters:
    - int: month
    - int: day
    - int: year
    
    Returns
    - string: the day of the week in capital letters
    
    Input Format
    A single line of input containing the space separated month, day and year, respectively, in MM DD YYYY format.
    
    Constraints
    - 2000 < year < 3000

---
+++ Java/Introduction/Java_Data_and_Time/JavaDateTime0.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        // Create a new calendar object
+        Calendar calendar = Calendar.getInstance();
+        
+        // Set the year, month, & day attributes/fields
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month-1); // 0-indexed (0: Jan, 1: Feb, 2: Mar, ..., 11: Dec)
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        
+        // Return day of the week
+        return calendar.getDisplayName(calendar.DAY_OF_WEEK,calendar.LONG,Locale.US).toUpperCase();
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
