96e7591f7764a1cd94021aee874bf67a733eb784
Chisom Anumba <chisom213@gmail.com>
2023-05-04 03:24:19 -0400

    Create JavaIfElse4.java

---
+++ Java/Introduction/Java_If-Else/JavaIfElse4.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+      int N = Integer.parseInt(bufferedReader.readLine().trim());
+    
+      System.out.println((N % 2 == 1 || N % 2 == 0 && N >= 6 && N <= 20) ? "Weird" : "Not Weird");
+
+      bufferedReader.close();
+  }
+}
