07b7c452ac1edfd8c1a512abd2eda4a76b5bf1a0
Chisom Anumba <chisom213@gmail.com>
2023-05-17 08:31:08 -0400

    Create Foundations7The1000thPowerOfAMatrix2.py

---
+++ Mathematics/Linear_Algebra_Foundations/Linear_Algebra_Foundations_#7-The_1000th_Power_of_a_Matrix/Foundations7The1000thPowerOfAMatrix2.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == "__main__":
+  n = 1000
+  A, B, C, D = [-2 - 3*(n-1), -9*n, n, 1 + 3*n]
+  print(A, B, C, D, sep = '\n')
