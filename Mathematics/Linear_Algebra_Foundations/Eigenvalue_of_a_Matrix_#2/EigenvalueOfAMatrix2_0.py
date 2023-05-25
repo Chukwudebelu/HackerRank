857057bca9537d8b5ba576b1042c93cf53c57b95
Chisom Anumba <chisom213@gmail.com>
2023-05-25 11:01:51 -0400

    Create EigenvalueOfAMatrix2_0.py
    Find the eigenvalues of the matrix:
    
                                             A = [1  2]
                                                    [2  4]
    
    https://s3.amazonaws.com/hr-challenge-images/0/1445184257-2150ce1c5f-Screenshotfrom2015-10-18213408.png
    
    Your answer should have the eigenvalues separated by a single line resembling this(with smaller value coming first):
    
    5
    6

---
+++ Mathematics/Linear_Algebra_Foundations/Eigenvalue_of_a_Matrix_#2/EigenvalueOfAMatrix2_0.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def determinant(A: list[list[int]]) -> int:
+  return A[0][0]*A[1][1] - A[1][0]*A[0][1]
+
+def matrixSubtract(B, A):
+  C = [[0, 0], [0, 0]]
+  for i in range(len(B)):
+    for j in range(len(B[0])):
+      C[i][j] = B[i][j] - A[i][j]
+  return C
+
+def scalarMultiply(A, k: int):
+  Ak = [[0, 0], [0, 0]]
+  for i in range(len(A)):
+    for j in range(len(A[0])):
+      Ak[i][j] = k*A[i][j]
+  return Ak
+
+
+if __name__ == "__main__":
+  A = [[1, 2], [2, 4]]
+  I2 = [[1, 0], [0, 1]] # 2 x 2 identity matrix
+
+  eigval = -10
+  lst_eigvals = []
+  
+  while (eigval < 10):
+    val = determinant(matrixSubtract(scalarMultiply(I2, eigval), A))
+    if (val == 0): # |lambdaI - A| = 0
+      lst_eigvals += [eigval]
+    eigval += 1
+      
+  print(*lst_eigvals, sep='\n')
