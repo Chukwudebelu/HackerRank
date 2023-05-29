6bb17a4014ce0ed96e9c296279a3e848acaee7e0
Chisom Anumba <chisom213@gmail.com>
2023-05-29 05:32:52 -0400

    Create ItertoolsCompbinations()0.py
    itertools.combinations(iterable, r) [https://docs.python.org/2/library/itertools.html#itertools.combinations]
    This tool returns the r length subsequences of elements from the input iterable.
    
    Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
    
    Task
    You are given a string S.
    Your task is to print all possible combinations, up to size k, of the string in lexicographic sorted order.
    
    Input Format
    A single line containing the string S and integer value k separated by a space.
    
    Constraints
    - 0 < k ≤ len(S)
    - The string contains only UPPERCASE characters.
    
    Output Format
    Print the different combinations of string S on separate lines.

---
+++ Python/Itertools/itertools.combinations()/ItertoolsCompbinations()0.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations
+
+if __name__ == "__main__":
+    S, k = input().split()
+    
+    for j in range(1,int(k)+1):
+        print(*[''.join(list(i)) for i in list(combinations(sorted(S), j))], sep = '\n')
