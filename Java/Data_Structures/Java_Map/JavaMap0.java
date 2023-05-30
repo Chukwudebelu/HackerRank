3fac047f4f5e7821ddeaddd9cc8ac2cb9688957b
Chisom Anumba <chisom213@gmail.com>
2023-05-30 06:42:40 -0400

    Create JavaMap0.java
    You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person.
    
    Input Format
    The first line will have an integer n denoting the number of entries in the phone book. Each entry consists of two lines: a name and the corresponding phone number.
    
    After these, there will be some queries. Each query will contain a person's name. Read the queries until end-of-file.
    
    Constraints
    A person's name consists of only lower-case English letters and it may be in the format 'first-name last-name' or in the format 'first-name'. Each phone number has exactly 8 digits without any leading zeros.
    - 1 ≤ n ≤ 100000
    - 1 ≤ Query ≤ 100000
    
    Output Format
    For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number. See sample output for the exact format.
    
    To make the problem easier, we provided a portion of the code in the editor. You can either complete that code or write completely on your own.

---
+++ Java/Data_Structures/Java_Map/JavaMap0.java
@@ -0,0 +1,34 @@
+//!/bin/java
+// Complete this code or write your own from scratch
+import java.util.*;
+
+class Solution {
+	public static void main(String []args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.nextLine();
+    
+    // Create a HashMap object - {String=Integer} key/value pairs
+    HashMap<String, Integer> phonebook = new HashMap<String, Integer>();
+    
+    // Insert the name/phone pairs into HashMap
+		for (int i = 0; i < n; i++) {
+			String name = in.nextLine();
+			int phone = in.nextInt();
+      phonebook.put(name, phone);
+			in.nextLine();
+		}
+    
+		while (in.hasNext()) {
+			String s = in.nextLine();
+      if (phonebook.get(s) != null) {
+        System.out.println(s + "=" + phonebook.get(s));
+      } else {
+         System.out.println("Not found");
+      }
+		}
+    
+    // Close Scanner object
+    in.close();
+	}
+}
