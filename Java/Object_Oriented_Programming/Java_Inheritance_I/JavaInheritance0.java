ab21024a43e6d5e5601dca0a6b7a106d47414073
Chisom Anumba <chisom213@gmail.com>
2023-05-31 16:33:30 -0400

    Create JavaInheritance0.java
    Using inheritance, one class can acquire the properties of others. Consider the following Animal class:
    
    class Animal{
        void walk(){
            System.out.println("I am walking");
        }
    }
    
    This class has only one method, walk. Next, we want to create a Bird class that also has a fly method. We do this using extends keyword:
    
    class Bird extends Animal {
        void fly() {
            System.out.println("I am flying");
        }
    }
    
    Finally, we can create a Bird object that can both fly and walk.
    
    public class Solution{
       public static void main(String[] args){
    
          Bird bird = new Bird();
          bird.walk();
          bird.fly();
       }
    }
    
    The above code will print:
    
    I am walking
    I am flying
    
    This means that a Bird object has all the properties that an Animal object has, as well as some additional unique properties.
    
    The code above is provided for you in your editor. You must add a sing method to the Bird class, then modify the main method accordingly so that the code prints the following lines:
    
    I am walking
    I am flying
    I am singing

---
+++ Java/Object_Oriented_Programming/Java_Inheritance_I/JavaInheritance0.java
@@ -0,0 +1,26 @@
+//!bin/java
+class Animal {
+  void walk() {
+    System.out.println("I am walking");
+  }
+}
+
+class Bird extends Animal {
+  void fly() {
+    System.out.println("I am flying");
+  }
+  
+  void sing() {
+    System.out.println("I am singing");
+  }
+}
+
+public class Solution {
+  public static void main(String[] args) {
+    // Create a Bird object
+    Bird bird = new Bird();
+    bird.walk();
+    bird.fly();
+    bird.sing();
+  }
+}
