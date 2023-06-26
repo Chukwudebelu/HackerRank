dd75057d203c30f06afda6f34a9d1c70f1d4fc97
Chisom Anumba <chisom213@gmail.com>
2023-06-26 13:05:33 -0400

    Create VarargsSimpleAddition3.java

---
+++ Java/Advanced/Java_Varargs_-_Simple_Addition/VarargsSimpleAddition3.java
@@ -0,0 +1,53 @@
+//!/bin/java
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+class Add {
+  public void add(int... n) {
+    int sum = 0; String display_ = "";
+    for (int j = 0; j < n.length; j++) {
+      sum += n[j];
+      display_ += ((j < n.length-1) ? n[j] + "+" : n[j] + "=");
+    }
+    System.out.println(display_ + sum);
+  }
+}
+
+
+public class Solution {
+  public static void main(String[] args) {
+    try {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n1 = Integer.parseInt(br.readLine());
+      int n2 = Integer.parseInt(br.readLine());
+      int n3 = Integer.parseInt(br.readLine());
+      int n4 = Integer.parseInt(br.readLine());
+      int n5 = Integer.parseInt(br.readLine());
+      int n6 = Integer.parseInt(br.readLine());
+      Add ob = new Add();
+      ob.add(n1,n2);
+      ob.add(n1,n2,n3);
+      ob.add(n1,n2,n3,n4,n5);	
+      ob.add(n1,n2,n3,n4,n5,n6);
+
+      Method[] methods = Add.class.getDeclaredMethods();
+      Set<String> set = new HashSet<>();
+      boolean overload = false;
+      for (int i = 0; i < methods.length; i++) {
+        if (set.contains(methods[i].getName())) {
+          overload = true;
+          break;
+        }
+        set.add(methods[i].getName());
+			}
+
+      // Check for Method OVERLOADING
+			if (overload) {
+        throw new Exception("Overloading not allowed");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
