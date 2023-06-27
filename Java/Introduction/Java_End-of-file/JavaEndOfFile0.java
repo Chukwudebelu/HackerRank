372521346fff186d02252e024f5f67a40e936dbe
Chisom Anumba <chisom213@gmail.com>
2023-06-27 11:28:56 -0400

    Update JavaEndOfFile0.java

--- Java/Introduction/Java_End-of-file/JavaEndOfFile0.java
+++ Java/Introduction/Java_End-of-file/JavaEndOfFile0.java
@@ -5,8 +5,8 @@ public class Solution {
     public static void main(String[] args) {        
         Scanner scan = new Scanner(System.in);
         
-        for (int i = 1; s.hasNextLine() == true; i++) {
-            String msg = s.nextLine();
+        for (int i = 1; scan.hasNextLine() == true; i++) {
+            String msg = scan.nextLine();
             System.out.println(i + " " + msg);
         }
 
