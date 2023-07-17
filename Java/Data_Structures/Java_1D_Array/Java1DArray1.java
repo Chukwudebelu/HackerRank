ee2dae7f7a555a00754325b45ee90b186bd7856e
Chisom Anumba <chisom213@gmail.com>
2023-07-17 12:38:46 -0400

    Create Java1DArray1.java

---
+++ Java/Data_Structures/Java_1D_Array/Java1DArray1.java
@@ -0,0 +1,20 @@
+//!/bin/java
+
+public class Solution {
+  public static void main(String[] args) {
+    java.util.Scanner s = new java.util.Scanner(System.in);
+    int n = Integer.parseInt(s.next());
+    int a[] = new int[n];
+    
+    // Use a for loop to insert #s in the array
+    for (int j = 0; j < n; j++) {
+      a[j] = Integer.parseInt(s.next());
+    }
+    s.close(); // Close Scanner object
+
+    // Prints each sequential element in array a
+    for (int i = 0; i < a.length; i++) {
+      System.out.println(a[i]);
+    }
+  }
+}
