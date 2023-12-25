94d335aeddb21b322a4934217be5672213ba5ee1
Chisom Anumba <chisom213@gmail.com>
2023-12-25 05:28:49 -0500

    Create AnyOrAll1.py

---
+++ Python/Built-Ins/Any_or_All/AnyOrAll1.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def isPalindromic(x: int) -> bool:
+    original_num = x
+    reverse = 0
+    while x:
+        digit = x % 10
+        reverse = reverse*10 + digit
+        x //= 10
+    return (original_num == reverse)
+    
+    
+if __name__ == "__main__":
+    _ = int(input())
+    lst = map(int, input().split(' '))
+    all_positive = all([*map(lambda x: x > 0, lst)])
+    any_palindrome = any(list(map(isPalindromic, lst)))
+    print(all_positive and any_palindrome)
