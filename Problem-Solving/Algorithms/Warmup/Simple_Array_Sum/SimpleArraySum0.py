ef3703eba3ab7af8f469afec70ad05c25b8be587
Chisom Anumba <chisom213@gmail.com>
2024-01-24 13:12:38 -0500

    Create SimpleArraySum0.py
    Given an array of integers, find the sum of its elements.
    
    For example, if the array ar = [1, 2, 3], 1 + 2 + 3 = 6, so return 6.
    
    Function Description
    Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
    
    simpleArraySum has the following parameter(s):
    • ar: an array of integers
    
    Input Format
    The first line contains an integer, n, denoting the size of the array.
    The second line contains n space-separated integers representing the array's elements.
    
    Constraints
    • 0 ≤ n, ar[i] ≤ 1000

---
+++ Problem-Solving/Algorithms/Warmup/Simple_Array_Sum/SimpleArraySum0.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum_ = 0
+    for i in ar:
+        sum_ += i
+    return sum_
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
