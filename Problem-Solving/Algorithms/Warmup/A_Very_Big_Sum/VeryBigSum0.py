d67ce57b1e1c4189fd8fa28486ae17244e2e7046
Chisom Anumba <chisom213@gmail.com>
2024-01-26 12:45:50 -0500

    Create VeryBigSum0.py
    In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
    
    Function Description
    Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
    
    aVeryBigSum has the following parameter(s):
    • int ar[n]: an array of integers n.
    
    Return
    • long: the sum of all array elements
    
    Input Format
    The first line of the input consists of an integer n.
    The next line contains n space-separated integers contained in the array.
    
    Output Format
    Return the integer sum of the elements in the array.
    
    Constraints
    • 1 ≤ n ≤ 10
    • 0 ≤ ar[i] ≤ 10¹⁰

---
+++ Problem-Solving/Algorithms/Warmup/A_Very_Big_Sum/VeryBigSum0.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar: int) -> int:
+    # Write your code here
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
