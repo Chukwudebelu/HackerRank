baad17b885906771070f9d89b6c2c6b04c7191dc
Chisom Anumba <chisom213@gmail.com>
2024-02-02 05:57:28 -0500

    Create BirthdayCakeCandles0.py
    You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
    
    Example
    candles = [4, 4, 1, 3]
    The maximum height candles are 4 units high. There are 2 of them, so return 2.
    
    Function Description
    Complete the function birthdayCakeCandles in the editor below.
    birthdayCakeCandles has the following parameter(s):
    • int candles[n]: the candle heights
    
    Returns
    • int: the number of candles that are tallest
    
    Input Format
    The first line contains a single integer, n, the size of candles[].
    The second line contains n space-separated integers, where each integer  describes the height of candles[i].
    
    Constraints
    • 1 ≤ n ≤ 10⁵
    • 1 ≤ candles[i] ≤ 10⁷

---
+++ Problem-Solving/Algorithms/Warmup/Birthday_Cake_Candles/BirthdayCakeCandles0.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles: list) -> int:
+    count_max = 0
+    max_height = max(candles)
+    for i in candles:
+        if (i == max_height):
+            count_max += 1
+    return count_max
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
