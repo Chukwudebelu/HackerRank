20d513f9291215ef80eb1b04bde5295159af4bd3
Chisom Anumba <chisom213@gmail.com>
2024-06-10 03:55:06 -0400

    Create NumberGroups1.py

---
+++ Mathematics/Algebra/Number_Groups/NumberGroups1.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+#
+# Complete the 'sumOfGroup' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER k as parameter.
+#
+
+def sumOfGroup(k: int) -> int:
+    return pow(k, 3)
+
+if __name__ == '__main__':
+    k = int(input().rstrip().lstrip)
+    answer = sumOfGroup(k)
+    print(answer, end = "\n") 
