cabd4786a30e96bfeb4838c3b0b3e2bc62bb30bf
Chisom Anumba <chisom213@gmail.com>
2024-07-12 05:42:47 -0400

    Create TellTheAverage0.py
    James is very naive in Mathematics, He always makes new things out of a given list of integers. Today he is given a list L, so he creates a value S out of it.
    
    S from a given list can be calculated as follows.
    
         value_of_S(list L)
         {
             while ((number of elements in L) > 1)
             {
                 a = L[0]
                 b = L[1]
                 delete L[1]
                 L[0] = a + b + ab
             }
             return L[0] % 1000000007
         }
    
    James has an ample amount of time, so he calculates the values of S for all the permutations of the given list L and finds their average value. Then he asks you to report that value.
    
    Input Format
    The first line contains an integer N, the number of integers in the list.
    The second line contains N integral values, L[0], ..., L[N-1], separated by single spaces.
    
    Output Format
    Print the floor [http://en.wikipedia.org/wiki/Floor_and_ceiling_functions] of the average value.
    
    Constraints
    • 2 ≤ N ≤ 10⁴
    • 2 ≤ L[i] ≤ 10⁶
    
    Sample Input
      2
      2 3
    
    Sample Output
      11
    
    Explanation
      The S value corresponding to the two permutations of the given list is L.

---
+++ Mathematics/Algebra/Tell_the_Average/TellTheAverage0.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+sys.setrecursionlimit(10**6)
+
+def value_of_S_recursive(L: list) -> int:
+    n = len(L)
+    if (n == 0):    # empty list: L = []
+        return 0
+    elif (n == 1):  # unary list: L = [a]
+        return L[0]
+    else:           # other lists: L = [a, b, ...]
+        a = L[0]
+        b = L[1]
+        del L[1]    # L.pop(1) or L.remove(L[1])
+        L[0] = (a + b + a*b) % (10**9 + 7)
+        return value_of_S_recursive(L)
+
+        
+if __name__ == "__main__":
+    N = int(input(''))
+    List = [int(_) for _ in input().split(' ')]
+    print(value_of_S(List))
