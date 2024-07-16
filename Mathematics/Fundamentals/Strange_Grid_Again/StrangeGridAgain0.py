63c3d2b2b85cff3245190a186f309b44f433de93
Chisom Anumba <chisom213@gmail.com>
2024-07-16 03:04:57 -0400

    Create StrangeGridAgain0.py
    A strange grid has been recovered from an old book. It has  columns and infinite number of rows. The bottom row is considered as the first row. First few rows of the grid are like this:
    
      ................................
    
      ................................
    
      20 22 24 26 28
    
      11 13 15 17 19
    
      10 12 14 16 18
    
       1  3  5  7  9
    
       0  2  4  6  8
    
    The grid grows upwards forever!
    Your task is to find the integer in cᵗʰ column in rᵗʰ row of the grid.
    
    Input Format
    There will be two integers r and c separated by a single space.
    
    Constraints
    • 1  ≤ r  ≤ 2 × 10⁹
    • 1 ≤ c ≤ 5
    
    Rows are indexed from bottom to top and columns are indexed from left to right.
    
    Output Format
    Output the answer in a single line.
    
    Sample Input 
      6 3
    
    Sample Output
      25
    
    Explanation
    The number in the 6ᵗʰ row and 3ʳᵈ column is 25.

---
+++ Mathematics/Fundamentals/Strange_Grid_Again/StrangeGridAgain0.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeGrid' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER r
+#  2. INTEGER c
+#
+
+def strangeGrid(r, c):
+    # if i = 1,2: single digit
+    # if i = 3,4: double digit that starts with 1
+    # if i = 5,6: double digit that starts with 2
+    # odd numbered row: even digits
+    # even numbered row: odd digits
+  
+    row1, row2 = range(0, 8+1, 2), range(1, 9+1, 2)
+    row1, row2 = list(map(str, row1)), list(map(str, row2))
+  
+    if r == 1 or r == 2:
+        n = ''
+        row = row1 if r == 1 else row2
+    elif r % 2 != 0:
+        n = r//2
+        row = row1
+    elif not r % 2:
+        n = (r-1)//2
+        row = row2
+ 
+    x = lambda a, k: str(n) + a[k]
+    for i in range(len(row)):
+        row[i] = x(row,i)
+    return row[c-1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    r = int(first_multiple_input[0])
+
+    c = int(first_multiple_input[1])
+
+    result = strangeGrid(r, c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
