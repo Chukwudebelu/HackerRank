1970f19fbe805d99c8e014c3d9e4d17d430022ad
Chisom Anumba <chisom213@gmail.com>
2024-07-18 16:01:31 -0400

    Create GetNodeValue2.py

---
+++ Problem-Solving/Data_Structures/Linked_Lists/Get_Node_Value/GetNodeValue2.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import os
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def getNode(llist: SinglyLinkedList, positionFromTail: int) -> int:
+    l = tuple()
+    while (llist):
+        l += (llist.data,)
+        llist = llist.next
+        
+    return l[-positionFromTail-1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
