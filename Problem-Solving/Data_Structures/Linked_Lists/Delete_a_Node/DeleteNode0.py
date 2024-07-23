9d03259b8277be6e32629bebb647ec268b67c90f
Chisom Anumba <chisom213@gmail.com>
2024-07-23 16:13:35 -0400

    Create DeleteNode0.py
    Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.
    
    Example
    llist = 0 → 1 → 2  → 3
    position = 2
    
    After removing the node at position 2, llist = 0 → 1 → 3.
    
    Function Description
    Complete the deleteNode function in the editor below.
    deleteNode has the following parameters:
    - SinglyLinkedListNode pointer llist: a reference to the head node in the list
    - int position: the position of the node to remove
    
    Returns
    - SinglyLinkedListNode pointer: a reference to the head of the modified list
    
    Input Format
    The first line of input contains an integer n, the number of elements in the linked list.
    Each of the next n lines contains an integer, the node data values in order.
    The last line contains an integer, position, the position of the node to delete.
    
    Constraints
    • 1 ≤ n ≤ 1000
    • 1 ≤ list[i] ≤ 1000, where list[i] is the iᵗʰ element of the linked list.
    
    Sample Input
      8
      20
      6
      2
      19
      7
      4
      15
      9
      3
    
    Sample Output
      20 6 2 7 4 15 9
    
    Explanation
    The original list is 20 → 6 → 2 → 19 → 7 → 4 → 15 → 9. After deleting the node at position 3, the list is 20 → 6 → 2 → 7 → 4 → 15 → 9.

---
+++ Problem-Solving/Data_Structures/Linked_Lists/Delete_a_Node/DeleteNode0.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import os
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'deleteNode' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+
+def deleteNode(llist: SinglyLinkedList, position: int) -> SinglyLinkedList:
+    if (position == 0):
+        llist = llist.next
+    else:
+        count = 0
+        curr = llist
+        while (curr is not None):
+            if (count == position-1):
+                curr.next = curr.next.next
+                break
+            curr = curr. next
+            count += 1
+    return llist
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
